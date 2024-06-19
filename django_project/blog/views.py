98dbf109f9279a536ea2cbad717fe632d3c8f05d
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-06-19 16:07:57 +0530

    first working version
    the blog website is working, there are two posts to begin with and a simple UI

---
+++ django_project/blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+posts = [
+    {
+        'author': 'Eizad Hamdan',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 18, 2024'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 19, 2024'
+    },
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
