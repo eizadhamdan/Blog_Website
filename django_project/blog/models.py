ebaae7f0c9f069ac615ddb36f491c0666a604bd9
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-06-20 00:11:07 +0530

    update views, admin and date-time format

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -9,5 +9,8 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    # alternative (it'll update the date & time when an object is created)
+    # date_posted = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,27 +1,10 @@
 from django.shortcuts import render
-
-
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Eizad Hamdan',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'June 18, 2024'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 19, 2024'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
